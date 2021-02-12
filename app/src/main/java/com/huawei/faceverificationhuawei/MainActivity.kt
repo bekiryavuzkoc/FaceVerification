28081cf3bbf264a4b91b23c4d3f74493848d54a7
bekiryavuzkoc <45243786+bekiryavuzkoc@users.noreply.github.com>
2021-02-12 16:01:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/huawei/faceverificationhuawei/MainActivity.kt
+++ app/src/main/java/com/huawei/faceverificationhuawei/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MLApplication.getInstance().apiKey = "CgB6e3x96IS7xJsGLUkxfg04VuvYtLqtXD7K7DI9NESSnl5pKH570YL4D1ZUKcuzVa+N2FSsk4cLnIUPLM4Nnz+9"
+        MLApplication.getInstance().apiKey = "your_api_key"
 
         createAndSetAnalyzer()
 
